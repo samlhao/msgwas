3d0e532823344e5cb4cd3d63ae7170ac3745cf5f
shao11 <shao11@c035.rockfish.cluster>
2022-09-23 08:55:34 -0400

    cluster labeling

--- R/retina_singlecell.R
+++ R/retina_singlecell.R
@@ -153,16 +153,82 @@ DefaultAssay(retina_combined) <- "RNA"
 retina_markers <- FindAllMarkers(retina_combined, only.pos = TRUE,
                                  min.pct = 0.25, logfc.threshold = 0.25)
 saveRDS(retina_markers, file = "data/processed/retina_markers.rds")
-markers_top2 <- retina_markers %>%
+markers_top <- retina_markers %>%
   group_by(cluster) %>%
-  slice_max(n = 2, order_by = avg_log2FC)
+  slice_max(n = 5, order_by = avg_log2FC)
 cluster_markers <- retina_markers %>%
   group_by(cluster) %>%
   slice_max(n = 1, order_by = avg_log2FC)
-write.csv(x = markers_top2, file = "data/processed/retina_markers.csv")
+write.csv(x = markers_top, file = "data/processed/retina_markers.csv")
 
 pdf(file = "figures/retina_featureplot.pdf", width = 10, height = 10)
 FeaturePlot(retina_combined,
             features = c("ARL4D","PPEF2","CRABP1","RHO","RP11-184I16.4","IMPG1","PDE6H","PVALB","GRIK1","NEFL","WIF1","TTR","TRPM3","MEG3","PDE6H","PCDH9","CD74","CNTNAP2","SPARCL1","VSX1"),
             min.cutoff = "q9")
 dev.off()
+# rod PR
+View(retina_markers %>%
+  filter(gene == 'RHO'|gene == 'CNGA1'|gene == 'PDE6A'))
+FeaturePlot(retina_combined,
+            features = c("RHO", "PDE6A"),
+            min.cutoff= "q9",
+            blend = T,
+            label = T)
+# cone PR
+View(retina_markers %>%
+  filter(gene == 'ARR3'|gene == 'GNGT2'|gene == 'GUCA1C'))
+# Muller glia
+View(retina_markers %>%
+  filter(gene == 'RLBP1'|gene == 'CRALBP'))
+# astrocytes
+View(retina_markers %>%
+       filter(gene == 'GFAP'))
+FeaturePlot(retina_combined,
+            features = c("GFAP"),
+            min.cutoff= "q9",
+            label = T)
+# microglia
+View(retina_markers %>%
+       filter(gene == 'HLA-DPA1'|gene == 'HLA-DPB1'|gene == 'HLA-DRA'))
+FeaturePlot(retina_combined,
+            features = c("HLA-DRA"),
+            min.cutoff = "q9",
+            label = T)
+# bipolar cells
+View(retina_markers %>%
+       filter(gene == 'VSX2'|gene == 'OTX2'))
+# RGC
+View(retina_markers %>%
+       filter(gene == 'NEFL'|gene == 'GAP43'|gene == 'SNCG'))
+# amacrine
+View(retina_markers %>%
+       filter(gene == 'GAD1'|gene == 'CALB1'|gene == 'CHAT'))
+# horizontal cells
+View(retina_markers %>%
+       filter(gene == 'ONECUT1'|gene == 'ONECUT2'))
+# assign cluster IDs
+retina_combined <- RenameIdents(retina_combined,
+                                `0` = "Rod PR (RHO, CNGA1, PDE6A)/bipolar cells (OTX2)",
+                                `1` = "Rod PR (PDE6A)",
+                                `2` = "Muller glia (RLBP1)",
+                                `3` = "Rod PR (RHO, CNGA1, PDE6A)",
+                                `4` = "Rod PR (RHO, CNGA1)",
+                                `5` = "Rod PR (RHO, CNGA1)",
+                                `6` = "Cone PR (ARR3)/bipolar cells (OTX2)",
+                                `7` = "RGC (SNCG, NEFL)",
+                                `8` = "bipolar cells (VSX2, OTX2)",
+                                `9` = "bipolar cells (VSX2, OTX2)",
+                                `10` = "bipolar cells (VSX1, OTX2)",
+                                `11` = "Muller glia (RLBP1)/Astrocytes (GFAP)/bipolar cells (VSX2)",
+                                `12` = "RGC (SNCG, NEFL)",
+                                `13` = "Muller glia (RLBP1)/bipolar cells (VSX2)",
+                                `14` = "bipolar cells (VSX2, OTX2)",
+                                `15` = "Muller glia (RLBP1)",
+                                `16` = "amacrine (GAD1)",
+                                `17` = "Cone PR (ARR3, GUCA1C, GNGT2)/amacrine (CALB1)",
+                                `18` = "bipolar cells (VSX2, OTX2)/amacrine (CALB1)",
+                                `19` = "microglia (HLA-DPA1, HLA-DPB1, HLA-DRA)",
+                                `20` = "RGC (SNCG)/amacrine (CALB1)/horizontal (ONECUT1, ONECUT2)",
+                                `21` = "Astrocytes (GFAP)",
+                                `22` = "bipolar cells (VSX1, VSX2, OTX2)/RGC (SNCG, NEFL)")
+
