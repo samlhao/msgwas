23dbcb02fa09f46fccbdc24ab2c6d4a602eea8b4
shao11 <shao11@login01.rockfish.cluster>
2022-10-16 20:52:59 -0400

    select cells based on published assignments

--- R/brain_singlecell.R
+++ R/brain_singlecell.R
@@ -3,35 +3,66 @@
 # It then performs an integrated analysis.
 
 # load libraries
-library(Seurat)
 library(patchwork)
 library(data.table)
 library(dplyr)
 library(clustree)
+library(readxl)
+library(stringr)
+library(Seurat)
+library(SeuratObject)
 
 setwd("/home/shao11/data_kfitzg13/msgwas_shao11")
 
-# load schirmer dataset
-schirmer_raw <- Read10X(data.dir = "data/sc_data/brain/schirmer/run_cellranger_aggr/schirmer_aggr/outs/count/filtered_feature_bc_matrix/")
-schirmer_data <- CreateSeuratObject(counts = schirmer_raw,
-                                    project = "schirmer",
-                                    min.cells = 5,
-                                    min.features = 500)
+# load schirmer dataset per sample
+cellranger_files <- Sys.glob(paths = "data/sc_data/brain/schirmer/run_cellranger_count/SRR*/outs/raw_feature_bc_matrix/")
 
-# calculate ribosomal and mt
-schirmer_data[["percent.mt"]] <- PercentageFeatureSet(schirmer_data, pattern = "^MT-")
-schirmer_data[["percent.rbp"]] <- PercentageFeatureSet(schirmer_data, pattern = "^RP[SL]")
+cellranger_files <- data.frame(datadir = cellranger_files, stringsAsFactors = F) %>%
+  mutate(Run = str_extract(string = datadir, pattern = "SRR[^/]+"))
+sra_runtable <- read.csv(file = "data/sc_data/brain/schirmer/schirmer_SRARunTable.txt")
+sra_runtable <- left_join(sra_runtable, cellranger_files, by = "Run")
+raw_files <- sapply(sra_runtable$datadir, Read10X)
+data_files <- sapply(raw_files, CreateSeuratObject)
+schirmer_data <- merge(data_files[[1]], y = data_files[2:21], add.cell.ids = sra_runtable$Isolate)
 
-# violin plots
-pdf(file = "code/msgwas/figures/schirmer_vlnplot.pdf", width = 11, height = 8)
-VlnPlot(schirmer_data, features = c("nFeature_RNA","nCount_RNA","percent.mt","percent.rbp"), ncol = 4)
-dev.off()
+# load cell assignments
+schirmer_cell_types <- read_excel("data/sc_data/brain/schirmer/schirmer_supplementary_tables.xlsx", sheet = "Supplementary Table 2")
+schirmer_cell_types <- schirmer_cell_types %>%
+  mutate(cellUMI = str_extract(cell, "[^_]*")) %>%
+  mutate(sampleUMI = paste(sample, cellUMI, sep = "_"))
+rownames(schirmer_cell_types) <- schirmer_cell_types$sampleUMI
 
-# filter datasets to remove dying cells (mt contam), low-qual cells (few genes)
-# and putative multiplets (abberantly high gene count)
-schirmer_data <- subset(schirmer_data, subset = nFeature_RNA > 200 & nFeature_RNA < 2500 & percent.mt < 10)
+labeled_schirmer_data <- schirmer_data[,colnames(schirmer_data) %in% schirmer_cell_types$sampleUMI]
+View(schirmer_cell_types[!(schirmer_cell_types$cell %in% colnames(schirmer_data)),])
 
-# normalize, find features, cluster
+schirmer_data <- AddMetaData(object = labeled_schirmer_data, metadata = schirmer_cell_types)
+saveRDS(schirmer_data, file = "data/processed/schirmer.rds")
+# ----------------------------
+# ANALYSIS
+# schirmer_raw <- Read10X(data.dir = "data/sc_data/brain/schirmer/run_cellranger_aggr/schirmer_aggr/outs/count/filtered_feature_bc_matrix/")
+# schirmer_data <- CreateSeuratObject(counts = schirmer_raw,
+#                                     project = "schirmer",
+#                                     min.cells = 5,
+#                                     min.features = 500)
+# 
+# 
+# # calculate ribosomal and mt
+# schirmer_data[["percent.mt"]] <- PercentageFeatureSet(schirmer_data, pattern = "^MT-")
+# schirmer_data[["percent.rbp"]] <- PercentageFeatureSet(schirmer_data, pattern = "^RP[SL]")
+# 
+# # violin plots
+# pdf(file = "code/msgwas/figures/schirmer_vlnplot.pdf", width = 11, height = 8)
+# VlnPlot(schirmer_data, features = c("nFeature_RNA","nCount_RNA","percent.mt","percent.rbp"), ncol = 4)
+# dev.off()
+# 
+# # filter out mitochondrial genes (outside nucleus)
+schirmer_data <- schirmer_data[!grepl("^MT-", rownames(schirmer_data)),]
+# 
+# # filter out cells with transcript count less than 1000
+# schirmer_data <- subset(schirmer_data, subset = nCount_RNA > 1000 & nFeature_RNA > 500)
+# 
+# 
+# # normalize, find features, cluster
 schirmer_data <- NormalizeData(schirmer_data)
 schirmer_data <- FindVariableFeatures(schirmer_data, selection.method = "vst", nfeatures = 2000)
 all.genes <- rownames(schirmer_data)
@@ -39,12 +70,15 @@ schirmer_data <- ScaleData(schirmer_data, features = all.genes)
 schirmer_data <- RunPCA(schirmer_data, features = VariableFeatures(object = schirmer_data))
 ElbowPlot(schirmer_data)
 schirmer_data <- FindNeighbors(schirmer_data, reduction = "pca", dims = 1:11)
-schirmer_data <- FindClusters(schirmer_data, resolution = c(0.4, 0.5, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8))
-pdf(file = "code/msgwas/figures/schirmer_clustree.pdf", width = 11, height = 8)
-clustree(schirmer_data)
-dev.off()
-# 0.6 gives 22 clusters
-schirmer_data$seurat_clusters <- schirmer_data$RNA_snn_res.0.6
+# resolution gives 22 clusters
+schirmer_data <- FindClusters(schirmer_data, resolution = c(0.5))
+#-------------------------------
+# clustree analysis to check appropriate resolution
+# pdf(file = "code/msgwas/figures/schirmer_clustree.pdf", width = 11, height = 8)
+# clustree(schirmer_data)
+# dev.off()
+# schirmer_data$seurat_clusters <- schirmer_data$RNA_snn_res.0.6
+#--------------------------------
 schirmer_data <- RunTSNE(schirmer_data, dims = 1:11)
 pdf(file = "code/msgwas/figures/tSNE_schirmer.pdf", width = 11, height = 8)
 DimPlot(schirmer_data, label = T)
